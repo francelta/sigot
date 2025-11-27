91e930ecf8abac975d9790b7e2096f3a7012bdee
franCarrascoDLM <fran.carrasco@dataleanmakers.es>
2025-11-27 09:38:30 +0100

    Add root health check endpoint

--- sigot/boot/urls.py
+++ sigot/boot/urls.py
@@ -6,8 +6,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({
+        'status': 'ok',
+        'service': 'SIGOT Backend API',
+        'endpoints': {
+            'api': '/api/',
+            'admin': '/admin/',
+        }
+    })
 
 urlpatterns = [
+    path('', health_check, name='root'),
     path('admin/', admin.site.urls),
     # API endpoints
     path('api/', include(('sigot.infrastructure.api.urls', 'api'), namespace='api')),
